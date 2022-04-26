d735a40d49f22fea9bf9265e1a33138528b0ab95
Carey Lzh <lzh.carey@gmail.com>
2022-04-27 01:25:50 +0800

    feat(Spring): precedence of autowiring
    another method is autowiring by name: when autowiring your dependency, can also reference the name of the dependency instance generated during runtime. See BinarySearchImpl
    
    However, as you can see in debug, the @Primary decorator in QuickSortAlgo takes precedence over autowiring by name - dependency used was QuickSortAlgo even though BinarySearchImpl specifies the instance bubbleSortAlgo

--- src/main/java/com/spring28minutescourse/demo/BinarySearchImpl.java
+++ src/main/java/com/spring28minutescourse/demo/BinarySearchImpl.java
@@ -7,20 +7,21 @@ import org.springframework.stereotype.Component;
 public class BinarySearchImpl {
 
     @Autowired
-    private SortingAlgo sortingAlgo;
+    private SortingAlgo bubbleSortAlgo; //autowiring by name: you can also perform autowiring by referencing the name of the dependency instance generated during runtime
 
-    public BinarySearchImpl(SortingAlgo sortingAlgo) {
-        super();
-        this.sortingAlgo = sortingAlgo;
-    }
+    //this thing is actually optional for dependency injection in Spring. somehow Spring knows that 
+//    public BinarySearchImpl(SortingAlgo sortingAlgo) {
+//        super();
+//        this.sortingAlgo = sortingAlgo;
+//    }
 
 
     public int binarySearch(int[] numbers, int numberToSearchFor) {
         //Implement the sorting algo here, which should be a variable dependency of binary search
 
 
-        int[] sortedNumber = sortingAlgo.sort(numbers);
-        System.out.println(sortingAlgo);
+        int[] sortedNumber = bubbleSortAlgo.sort(numbers);
+        System.out.println(bubbleSortAlgo);
 
         //Search an array
         //Return the result(index of the target element)
