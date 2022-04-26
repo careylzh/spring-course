6e980e1d8a00a9720558ae0723c6f954323aa097
Carey Lzh <lzh.carey@gmail.com>
2022-04-27 01:35:21 +0800

    fix(Spring): undo autowiring by name

--- src/main/java/com/spring28minutescourse/demo/BinarySearchImpl.java
+++ src/main/java/com/spring28minutescourse/demo/BinarySearchImpl.java
@@ -7,21 +7,21 @@ import org.springframework.stereotype.Component;
 public class BinarySearchImpl {
 
     @Autowired
-    private SortingAlgo bubbleSortAlgo; //autowiring by name: you can also perform autowiring by referencing the name of the dependency instance generated during runtime
+    private SortingAlgo sortingAlgo; //autowiring by name: you can also perform autowiring by referencing the name of the dependency instance generated during runtime
 
-    //this thing is actually optional for dependency injection in Spring. somehow Spring knows that 
-//    public BinarySearchImpl(SortingAlgo sortingAlgo) {
-//        super();
-//        this.sortingAlgo = sortingAlgo;
-//    }
+    //(prev concept on dependency injection using constructors vs setters): this thing is actually optional for dependency injection in Spring. somehow Spring knows that you will be injecting a dependency into BinarySearchImpl without defining the constructor
+    public BinarySearchImpl(SortingAlgo sortingAlgo) {
+        super();
+        this.sortingAlgo = sortingAlgo;
+    }
 
 
     public int binarySearch(int[] numbers, int numberToSearchFor) {
         //Implement the sorting algo here, which should be a variable dependency of binary search
 
 
-        int[] sortedNumber = bubbleSortAlgo.sort(numbers);
-        System.out.println(bubbleSortAlgo);
+        int[] sortedNumber = sortingAlgo.sort(numbers);
+        System.out.println(sortingAlgo);
 
         //Search an array
         //Return the result(index of the target element)
