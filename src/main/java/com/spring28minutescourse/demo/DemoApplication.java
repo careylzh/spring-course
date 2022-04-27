537236c01c1353bd91543bb662cb543708fd08c8
Carey Lzh <lzh.carey@gmail.com>
2022-04-27 14:12:20 +0800

    feat(bean scope): by default, beans follow singleton pattern
    one bean instance per context

--- src/main/java/com/spring28minutescourse/demo/DemoApplication.java
+++ src/main/java/com/spring28minutescourse/demo/DemoApplication.java
@@ -10,6 +10,11 @@ public class DemoApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext applicationContext = SpringApplication.run(DemoApplication.class, args);
         BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
+        BinarySearchImpl binarySearch2 = applicationContext.getBean(BinarySearchImpl.class);
+        System.out.println(binarySearch);
+        System.out.println(binarySearch2);
+        //Singleton: by default, one bean instance per context. Both instances of BinarySearchImpl point to the same instance.
+
         int result = binarySearch.binarySearch(new int[]{12, 4, 6}, 3);
         System.out.println(result);
     }
